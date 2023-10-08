403c38fb6c5291ad30e5625dd341ccc33b66e680
Çağrı <karpuzcagri@gmail.com>
2023-10-08 20:20:02 +0300

    Update integral.py

--- integral with python/integral.py
+++ integral with python/integral.py
@@ -1,14 +1,21 @@
 import sympy as smp
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 x = smp.symbols('x')
 
+print(smp.limit(smp.sin(x/2 + smp.sin(x)), x, smp.pi))
+
+print(smp.diff(((1 + smp.sin(x)) / (1 - smp.cos(x)))*2, x))
+
+#y = smp.sin(x)
+#print(x)
 
 
-print(x)
 
-print(smp.cos(x))
+#print(smp.sin(x))
 
-print(smp.sin(x))
+#z =  x**2
 
-print(smp.sin(180))
+#print(z.expand())
